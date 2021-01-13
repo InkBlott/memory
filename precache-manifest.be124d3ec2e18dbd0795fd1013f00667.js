8bfc2dcff44ea13fc30b48fc9ee58ffe28ac353c
Ink <bmantas@tutanota.com>
2021-01-13 16:57:27 +0200

    Updates

---
+++ precache-manifest.be124d3ec2e18dbd0795fd1013f00667.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452438722478ebe97dfded63a3889a13",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "c33ecfed179f71b6f465",
+    "url": "/memory/static/css/main.8722ba37.chunk.css"
+  },
+  {
+    "revision": "30198e240f23f34a9ed7",
+    "url": "/memory/static/js/2.ae6eb65d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory/static/js/2.ae6eb65d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33ecfed179f71b6f465",
+    "url": "/memory/static/js/main.fef3b6cc.chunk.js"
+  },
+  {
+    "revision": "62e0eae13c9afaa66ed5",
+    "url": "/memory/static/js/runtime-main.c4984c0b.js"
+  },
+  {
+    "revision": "a22a4449a852125157d718d905669b38",
+    "url": "/memory/static/media/Ball0.a22a4449.jpeg"
+  },
+  {
+    "revision": "5b933fdc92f4edf7054262368ce47973",
+    "url": "/memory/static/media/Ball1.5b933fdc.jpeg"
+  },
+  {
+    "revision": "109d06e88407ca92254016bdecf6503c",
+    "url": "/memory/static/media/Ball2.109d06e8.jpeg"
+  },
+  {
+    "revision": "f47eed5b74951f106b90806167380333",
+    "url": "/memory/static/media/Ball3.f47eed5b.jpeg"
+  },
+  {
+    "revision": "aa83c62057494d7eee3973af1a2c5961",
+    "url": "/memory/static/media/Ball4.aa83c620.jpeg"
+  },
+  {
+    "revision": "6e431f5360c29cb69654c382b3350178",
+    "url": "/memory/static/media/Ball5.6e431f53.jpeg"
+  },
+  {
+    "revision": "22c1ab3b9d0cd498579e7b97e205ad26",
+    "url": "/memory/static/media/Ball6.22c1ab3b.jpeg"
+  },
+  {
+    "revision": "5afc9499bb62418739fed3f87e8428b9",
+    "url": "/memory/static/media/Ball7.5afc9499.jpeg"
+  },
+  {
+    "revision": "bd6d86b960dfd8a6ad5af63c026042d7",
+    "url": "/memory/static/media/Ball8.bd6d86b9.jpeg"
+  },
+  {
+    "revision": "8e079537f62283097cf5198f1c509004",
+    "url": "/memory/static/media/Ball9.8e079537.jpeg"
+  }
+]);
